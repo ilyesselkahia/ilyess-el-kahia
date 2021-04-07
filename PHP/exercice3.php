c8fa9add2bc8c6a8e0a2186fd38cf463f5f3b6e3
ilyess el kahia <ilyess.elkahia@gmail.com>
2021-04-07 19:00:50 +0100

    Ajout exercice js

--- PHP/exercice3.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$monNom = 'el kahia';
-$monPrenom = 'ilyess';
-$monAge = '35 ans';
-echo "<br> $monNom <br> $monPrenom <br> $monAge<br>";
-?>
-
-
-
--- PHP/exercice7.php
+++
@@ -1,9 +0,0 @@
-<ul>
-    <?php
-    $tech = ['HTML', 'CSS', 'PHP', 'SQL', 'MYSQL'];
-
-    foreach ($tech as $liste) {
-        echo "<h1><li>$liste</li></h1>";
-    }
-    ?>
-</ul>
